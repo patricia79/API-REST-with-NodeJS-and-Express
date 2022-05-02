09a482755eb53a97461da5026e92a9e1b8e8ac3d
Patricia <waltraute@hotmail.com>
2022-05-03 00:03:37 +0200

    .get

--- app.js
+++ app.js
@@ -6,6 +6,17 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+//app.post('./playerPost', playerController);
+
+app.get('./playerGet'),(req, res) => {
+    try {
+      res.status(200).json({ name: `${req.params.name},
+    register_date: new Date`});
+    } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+  }
 app.listen(config.port, () => {
     console.log(`API REST en http://localhost:${config.port}/`);
   });
+   
