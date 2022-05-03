5645c448b928cc5c7b597bee8ab5edc50f043b2f
Patricia <waltraute@hotmail.com>
2022-05-03 10:04:22 +0200

    fer un player get

--- app.js
+++ app.js
@@ -8,15 +8,14 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 //app.post('./playerPost', playerController);
 
-app.get('./playerGet'),(req, res) => {
+app.get('/playerGet',(req, res) => {
     try {
-      res.status(200).json({ name: `${req.params.name},
-    register_date: new Date`});
+      res.status(200).json({ name: `${req.params.name}`});
     } catch (error) {
     res.status(500).json({ message: "Internal Server Error" });
   }
-  }
+  })
 app.listen(config.port, () => {
     console.log(`API REST en http://localhost:${config.port}/`);
-  });
-   
+  })
+// esto me da un undefined, lo sé
